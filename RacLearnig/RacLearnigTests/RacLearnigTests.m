cb5b6b00dba09b76d7ac27cd0f13117dc8036153
wen <zhangwen@angejia.com>
2016-12-14 16:28:44 +0800

    initial commit

---
+++ RacLearnig/RacLearnigTests/RacLearnigTests.m
@@ -0,0 +1,39 @@
+//
+//  RacLearnigTests.m
+//  RacLearnigTests
+//
+//  Created by agj on 16/12/14.
+//  Copyright © 2016年 agj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RacLearnigTests : XCTestCase
+
+@end
+
+@implementation RacLearnigTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
